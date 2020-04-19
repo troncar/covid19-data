9f126e7aa3e98c7c1e4e49b5d352e258dd7987d9
Nuria Tronco <nuria.tronco@gmail.com>
2020-04-19 18:38:37 -0500

    Updates

---
+++ precache-manifest.32d4af503ac4aae27ac8eca0240871e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35cd3c0af17ae4fe81c0db43b44f8347",
+    "url": "/covid19-data/index.html"
+  },
+  {
+    "revision": "e4e882fb2aa6e9d95b5e",
+    "url": "/covid19-data/static/css/main.56e64975.chunk.css"
+  },
+  {
+    "revision": "813d3fe4054575f765be",
+    "url": "/covid19-data/static/js/2.326c267b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19-data/static/js/2.326c267b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4e882fb2aa6e9d95b5e",
+    "url": "/covid19-data/static/js/main.75ea7369.chunk.js"
+  },
+  {
+    "revision": "ab07af977a05c913ec20",
+    "url": "/covid19-data/static/js/runtime-main.a7be2102.js"
+  }
+]);
